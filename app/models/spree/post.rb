12e3f0d080561dd687bbc8c3811ff86809ae4a76
Emanuele Barban <emanuelebarban@yahoo.it>
2013-03-11 17:31:39 +0100

    Clean post model and add FriendlyId

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -1,18 +1,21 @@
 module Spree
   class Post < ActiveRecord::Base
-  
+
     attr_accessible :title, :meta_keywords, :meta_description, :body, :permalink, :published, :published_at, :tag_list
+
     validates :title, :presence => true
     validates :permalink, :uniqueness => true
-    #scope :published, where(:published => true).order("created_at desc")
-    #scope :latest, arel_table[:published_at].lteq(DateTime.now)
+
     paginates_per 50
 
     acts_as_taggable
 
+    extend FriendlyId
+    friendly_id :title, use: :slugged, slug_column: :permalink
+
     scope :published, lambda { where(Post.arel_table[:published_at].lteq(DateTime.now)).order("created_at desc") }
 
-    def self.archives year, month=nil
+    def self.archives(year, month=nil)
       if month
         start_date = Date.new(year, month)
         end_date = start_date.end_of_month.next_day
